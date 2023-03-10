751a4e0d4680296b3fdfec92688c9f75135db608
LuxmanMadapatha <113989092+LuxmanMadapatha@users.noreply.github.com>
2023-03-10 17:18:54 +0530

    rename package

---
+++ src/assignment1/Exercise1a.java
@@ -0,0 +1,18 @@
+package assignment1;
+
+public class Exercise1a {
+
+  public static void main(String[] args) {
+    int x = 1;
+    while (x < 10) {
+
+      if (x > 3) {
+        System.out.println("big x: " + x);
+
+      }
+      x++;
+    }
+  }
+
+
+}
