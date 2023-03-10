b196e968f484fd1589aad4955ac2e9edfaf21f93
LuxmanMadapatha <113989092+LuxmanMadapatha@users.noreply.github.com>
2023-03-10 18:46:21 +0530

    Vehicle

---
+++ src/Vehicle/Car.java
@@ -0,0 +1,8 @@
+package Vehicle;
+
+public class Car extends Vehicle {
+
+  public Car(String make, String model, int year, String color, double price) {
+    super(make, model, year, color, price);
+  }
+}
