08372b59582c6fdc1ef0798d812f88ae87670f30
LuxmanMadapatha <113989092+LuxmanMadapatha@users.noreply.github.com>
2023-03-14 10:38:19 +0530

    Transaction update

--- src/bankaccount/Transaction.java
+++ src/bankaccount/Transaction.java
@@ -6,6 +6,38 @@ public class Transaction {
   double transactionAmount;
   String transactionType;
 
+  public String getTransactionID() {
+    return transactionID;
+  }
+
+  public void setTransactionID(String transactionID) {
+    this.transactionID = transactionID;
+  }
+
+  public String getTransactionDate() {
+    return transactionDate;
+  }
+
+  public void setTransactionDate(String transactionDate) {
+    this.transactionDate = transactionDate;
+  }
+
+  public double getTransactionAmount() {
+    return transactionAmount;
+  }
+
+  public void setTransactionAmount(double transactionAmount) {
+    this.transactionAmount = transactionAmount;
+  }
+
+  public String getTransactionType() {
+    return transactionType;
+  }
+
+  public void setTransactionType(String transactionType) {
+    this.transactionType = transactionType;
+  }
+
   public Transaction(String transactionID, String transactionDate, double transactionAmount,
       String transactionType) {
     this.transactionID = transactionID;
