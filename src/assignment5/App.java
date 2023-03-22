85cf393d954dee763617ae3ca5bbc5c2b043ef59
LuxmanMadapatha <113989092+LuxmanMadapatha@users.noreply.github.com>
2023-03-22 15:52:09 +0530

    Q4 update

---
+++ src/assignment5/App.java
@@ -0,0 +1,38 @@
+package assignment5;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+/*
+Requirements for collection type:
+Duplicate Strings may be required
+Key value pair not required
+Insert and delete method required
+Alpha sorting required
+Simple implementation
+ */
+
+public class App {
+
+  public static void main(String[] args) {
+
+    ArrayList<String> textArrayList = new ArrayList<String>();
+
+    Scanner myObj = new Scanner(System.in);
+    String name;
+
+    do {
+      System.out.println(
+          "Pls enter the set of names one by one:\n [or type exit to end the App]\n");
+      name = myObj.nextLine();
+      System.out.println(name.toString());
+      if (!name.equals("exit")) {
+        textArrayList.add(name);
+      }
+    } while (!name.equals("exit"));
+
+    Collections.sort(textArrayList);
+    System.out.println("arrayList" + ": " + textArrayList);
+  }
+}
