2c48f57a2489cfc3d4519083cc931cb6f20bab36
LuxmanMadapatha <113989092+LuxmanMadapatha@users.noreply.github.com>
2023-03-22 14:58:45 +0530

    Q4 update

---
+++ src/assignment4/App.java
@@ -0,0 +1,36 @@
+package assignment4;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+
+/*
+Requirements for collection type:
+Duplicates not needed
+Key value pair
+Get and put methods
+Speed
+key based alpha ordering not required
+ */
+public class App {
+
+  public static void main(String[] args) {
+
+    Map<String, Integer> textMap = new HashMap<String, Integer>();
+
+    Scanner myObj = new Scanner(System.in);
+    System.out.println("Pls enter a string:\n");
+    String name = myObj.nextLine();
+
+    for (int i = 0; i < name.length(); i++) {
+      if (!textMap.containsKey(name.substring(i, 1 + i))) {
+        textMap.put(name.substring(i, 1 + i), 1);
+
+      } else {
+        textMap.put(name.substring(i, 1 + i), textMap.get(name.substring(i, 1 + i)) + 1);
+      }
+    }
+    System.out.println(name + ": " + textMap);
+  }
+}
