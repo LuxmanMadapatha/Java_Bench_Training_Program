3bb128d3227de0d10ae89aab98210407b2c8dae8
LuxmanMadapatha <113989092+LuxmanMadapatha@users.noreply.github.com>
2023-03-28 16:20:46 +0530

    bank update

--- src/bankaccount/CreateAccount.java
+++ src/bankaccount/CreateAccount.java
@@ -9,25 +9,18 @@ public class CreateAccount {
 
     System.out.println("Enter name, account type, account no, and balance:");
 
-    // String input
     String name = myObj.nextLine();
     String type = myObj.nextLine();
-    //int account = myObj.nextInt();
 
-//    Scanner sc = new Scanner(System.in);
     int account = 0;
     do {
-      //System.out.println("Please enter a positive number!");
       while (!myObj.hasNextInt()) {
-        //System.out.println("That's not a number!");
         myObj.next(); // this is important!
       }
       account = myObj.nextInt();
     } while (account <= 0);
-    //System.out.println("Thank you! Got " + account);
     double balance = myObj.nextDouble();
 
-    // Output input by user
     System.out.println("Name: " + name);
     System.out.println("Account: " + account);
     System.out.println("Type: " + type);
